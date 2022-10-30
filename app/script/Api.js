79e7f025030ad5f3bf05b49021c761c31d4d32ce
Tomislav Busic <tomislav.busic89@gmail.com>
2022-10-30 09:52:12 +0100

    replace img

--- app/script/Api.js
+++ app/script/Api.js
@@ -1,31 +1,51 @@
-class Api {
-    constructor() {
+class Api{
+    constructor(){
 
-        this.fetchProperties();
+        this.fetchPropertiesForSale();
+        this.fetchPropertiesForRent();
     }
 
-    async fetchProperties() {
+    async fetchPropertiesForSale() {
         const options = {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
-                'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+                'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
             }
         };
 
         try {
-            const response = await fetch('https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4', options);
+            const response = await fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=200&sort=relevance', options);
             const data = await response.json();
-            let hits = data.hits;
+            let listings = data.listings;
 
-            return hits;
+            return listings;
 
         } catch(e) {
             console.log(e);
         }
     }
 
+    async fetchPropertiesForRent() {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
+                'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+            }
+        };
 
+        try {
+            const response = await fetch('https://realty-in-us.p.rapidapi.com/properties/v2/list-for-rent?city=New%20York%20City&state_code=NY&limit=200&offset=0&sort=relevance', options);
+            const data = await response.json();
+            let properties = data.properties;
+
+            return properties;
+
+        } catch(e) {
+            console.log(e);
+        }
+    }
 }
 
 
--- app/script/app.js
+++ app/script/app.js
@@ -7,31 +7,53 @@ let allProperties = [];
 
 
 async function fetchPropertiesData(){
-	let properties = new Api();
-	properties = await properties.fetchProperties();
+	let propRent = new Api();
+	propRent = await propRent.fetchPropertiesForRent();
 	
 	
-	displayAllProperties(properties);
-	console.log(properties);
+	displayAllProperties(propRent)
+	console.log(propRent);
 }
+ 
 
 const displayAllProperties = (data) => {
-	let propertys = data?.map(item => {
+	let properties = data?.map(item => {
 		template_property = template_cards_id.innerHTML;
 
-		template_property = template_property.replaceAll('${name}', item.contactName);
-		template_property = template_property.replaceAll('${img}', item.coverPhoto.url);
-		template_property = template_property.replaceAll('${purpose}', item.purpose);
-		template_property = template_property.replaceAll('${state}', item.state);
-		template_property = template_property.replaceAll('${rooms}', item.rooms);
-		template_property = template_property.replaceAll('${price}', item.price);
+		template_property = template_property.replaceAll('${id}', item.listing_id);
+		template_property = template_property.replaceAll('${city}', item.address.city);
+		template_property = template_property.replaceAll('${line}', item.address.line); 
+		template_property = template_property.replaceAll('${img}', item.photos['0'].href);
+		template_property = template_property.replaceAll('${imgOne}', item.photos['1']?.href);
+		template_property = template_property.replaceAll('${imgTwo}', item.photos['2']?.href);
+		template_property = template_property.replaceAll('${imgThree}', item.photos['3']?.href);
+		template_property = template_property.replaceAll('${imgFour}', item.photos['0'].href);  
+		template_property = template_property.replaceAll('${purpose}', changePurpose(item.prop_status));
+		template_property = template_property.replaceAll('${state}', item.listing_status);
+		template_property = template_property.replaceAll('${type}', item.prop_type); 
+		template_property = template_property.replaceAll('${price}', item.community?.price_max);  
 
 		return template_property;
 	}).join('');
 
-	placeForProperty.innerHTML = propertys;
+	placeForProperty.innerHTML = properties;
 } 
 
+const changePurpose = (purpose) => {
+	if (purpose === 'for_rent') {
+		purpose = 'for rent'
+	}
+
+	return purpose;
+}
+
+const replaceImg = (img) => {
+	let thisSrc  = img.getAttribute('src');
+	let parentImg = img.closest('.card');
+	let imgSrc = parentImg.querySelector('.bac-img');
+
+	imgSrc.src = thisSrc;
+}
 
 
 fetchPropertiesData();
